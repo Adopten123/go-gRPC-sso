4c5fc69b6cb7f0284b031ba768cfcee58b4040d0
Alexander Mikhailov <alexandermikhailov0505@gmail.com>
2026-01-20 12:25:35 +0300

    add service layer interfaces: UserSaver, UserProvider, AppProvider; add Auth struct and  add constructor

--- internal/services/auth/auth.go
+++ internal/services/auth/auth.go
@@ -1,1 +1,45 @@
 package auth
+
+import (
+	"context"
+	"go-gRPC-sso/internal/domain/models"
+	"log/slog"
+	"time"
+)
+
+type Auth struct {
+	logger       *slog.Logger
+	userSaver    UserSaver
+	userProvider UserProvider
+	appProvider  AppProvider
+	tokenTTL     time.Duration
+}
+
+type UserSaver interface {
+	SaveUser(context context.Context, email string, passHash []byte) (uid int64, err error)
+}
+
+type UserProvider interface {
+	User(context context.Context, email string) (models.User, error)
+	IsAdmin(context context.Context, email string) (bool, error)
+}
+
+type AppProvider interface {
+	App(context context.Context, appID int) (models.App, error)
+}
+
+func New(
+	logger *slog.Logger,
+	userSaver UserSaver,
+	userProvider UserProvider,
+	appProvider AppProvider,
+	tokenTTL time.Duration,
+) *Auth {
+	return &Auth{
+		userSaver:    userSaver,
+		userProvider: userProvider,
+		appProvider:  appProvider,
+		logger:       logger,
+		tokenTTL:     tokenTTL,
+	}
+}
