2d6a267ac5ba2df455ea5817eaf3208611ecbf8d
Alexander Mikhailov <alexandermikhailov0505@gmail.com>
2026-01-20 11:31:35 +0300

    update login handler and add login validator

--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -5,19 +5,44 @@ import (
 
 	ssov1 "github.com/Adopten123/go-protobufcontract-sso/gen/go/sso"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
+
+const (
+	emptyAppID  = 0
+	emptyString = ""
+)
+
+type AuthServer interface {
+	Login(context context.Context, email string, password string, appID int) (token string, err error)
+	RegisterNewUser(context context.Context, email string, password string) (userID int64, err error)
+	IsAdmin(context context.Context, userID int64) (isAdmin bool, err error)
+}
 
 type serverAPI struct {
 	ssov1.UnimplementedAuthServer
+	authServer AuthServer
 }
 
-func Register(gRPC *grpc.Server) {
-	ssov1.RegisterAuthServer(gRPC, &serverAPI{})
+func Register(gRPC *grpc.Server, authServer AuthServer) {
+	ssov1.RegisterAuthServer(gRPC, &serverAPI{
+		authServer: authServer,
+	})
 }
 
 func (s *serverAPI) Login(context context.Context, request *ssov1.LoginRequest) (*ssov1.LoginResponse, error) {
+	if err := validateLogin(request); err != nil {
+		return nil, err
+	}
+
+	token, err := s.authServer.Login(context, request.GetEmail(), request.GetPassword(), int(request.GetAppId()))
+	if err != nil {
+		return nil, status.Error(codes.Internal, "internal server error")
+	}
+
 	return &ssov1.LoginResponse{
-		Token: request.GetEmail(),
+		Token: token,
 	}, nil
 }
 
@@ -27,4 +52,17 @@ func (s *serverAPI) Register(context context.Context, request *ssov1.RegisterReq
 
 func (s *serverAPI) IsAdmin(context context.Context, request *ssov1.IsAdminRequest) (*ssov1.IsAdminResponse, error) {
 	panic("implement me")
+}
+
+func validateLogin(request *ssov1.LoginRequest) error {
+	if request.GetEmail() == emptyString {
+		return status.Error(codes.InvalidArgument, "email is required")
+	}
+	if request.GetPassword() == emptyString {
+		return status.Error(codes.InvalidArgument, "password is required")
+	}
+	if request.GetAppId() == emptyAppID {
+		return status.Error(codes.InvalidArgument, "appId is required")
+	}
+	return nil
 }
